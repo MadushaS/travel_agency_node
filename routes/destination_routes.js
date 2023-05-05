4b0e355f6f354a1e50505e4ba759d868fbbe44cd
_MadushaSandaruwan <44031016+MadushaS@users.noreply.github.com>
2023-05-05 17:32:17 +0530

    added dynamic routing to /destinations/city

--- routes/destination_routes.js
+++ routes/destination_routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const FileSystem = require('fs');
 
-router.get('/destinations/galle', (req, res) => {
-    res.sendFile('galle.html', { root: './public/destinations' });
+router.get('/destinations/:city', (req, res) => {
+    const city = req.params.city.toLowerCase();
+    if (FileSystem.existsSync(`./public/destinations/${city}.html`)) {
+        res.sendFile(`${city}.html`, { root: './public/destinations' });
+    }
+    else {
+        res.sendFile(`not_found.html`, { root: './public/destinations' });
+    }
 });
 
 module.exports = router;
